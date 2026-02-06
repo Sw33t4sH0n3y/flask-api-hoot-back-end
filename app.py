cad784c74848a925ff079d4b495c221cc48305ae
Ami <ami.dorsey@me.com>
2026-02-05 19:54:55 -0500

    app.py fix

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-from flask import Flask, jsonify, request, g
+from flask import Flask
 from dotenv import load_dotenv
-import jwt
-import bcrypt
-from auth_middleware import token_required
-from auth_blueprint import authentication_blueprint
-from hoots_blueprint import hoots_blueprint
 from auth_blueprint import authentication_blueprint
 from hoots_blueprint import hoots_blueprint
 from comments_blueprint import comments_blueprint
@@ -22,31 +17,31 @@ def index():
   return "Hello, world!"
   
 
-@app.route('/users')
-@token_required
-def users_index():
-    connection = get_db_connection()
-    cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    cursor.execute("SELECT id, username FROM users;")
-    users = cursor.fetchall()
-    connection.close()
-    return jsonify(users), 200
+# @app.route('/users')
+# @token_required
+# def users_index():
+#     connection = get_db_connection()
+#     cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+#     cursor.execute("SELECT id, username FROM users;")
+#     users = cursor.fetchall()
+#     connection.close()
+#     return jsonify(users), 200
 
-@app.route('/users/<user_id>')
-@token_required
-def users_show(user_id):
-    # If the user is looking for the details of another user, block the request
-    # Send a 403 status code to indicate that the user is unauthorized
-    if int(user_id) != g.user["id"]:
-        return jsonify({"err": "Unauthorized"}), 403
-    connection = get_db_connection()
-    cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    cursor.execute("SELECT id, username FROM users WHERE id = %s;", (user_id,))
-    user = cursor.fetchone()
-    connection.close()
-    if user is None:
-        return jsonify({"err": "User not found"}), 404
-    return jsonify(user), 200
+# @app.route('/users/<user_id>')
+# @token_required
+# def users_show(user_id):
+#     # If the user is looking for the details of another user, block the request
+#     # Send a 403 status code to indicate that the user is unauthorized
+#     if int(user_id) != g.user["id"]:
+#         return jsonify({"err": "Unauthorized"}), 403
+#     connection = get_db_connection()
+#     cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+#     cursor.execute("SELECT id, username FROM users WHERE id = %s;", (user_id,))
+#     user = cursor.fetchone()
+#     connection.close()
+#     if user is None:
+#         return jsonify({"err": "User not found"}), 404
+#     return jsonify(user), 200
 
 # Run our application, by default on port 5000
 app.run(debug=True)
